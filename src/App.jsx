7170d79da449c1e677231e05d12fbb6ec3377b67
Onyeabor Joel <earlmorningstar@gmail.com>
2024-01-02 15:24:52 +0100

    Second Commit.
    Updated Button, Input, newProject, NoProjectedSelected, Sidebar. (All Jsx).

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import Sidebar from "./Components/Sidebar";
-import NewProject from "./Components/NewProject";
-
+import { useState } from "react";
+import Sidebar from "./Components/Sidebar.jsx";
+import NewProject from "./Components/NewProject.jsx";
+import NoProjectSelected from "./Components/NoProjectSelected.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject()  {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    });
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
+
   return (
-    <>
-    <Sidebar />
-    <NewProject />
-      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar onStartAddProject={handleStartAddProject}/>
+      {content}
+    </main>
   );
 }
 
--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -1,4 +1,4 @@
-import Input from "./Input";
+import Input from "./Input.jsx";
 
 export default function NewProject(){
 const inputStyle = "w-64 px-2 py-1 rounded-sm bg-stone-200";
@@ -9,17 +9,26 @@ const inputStyle = "w-64 px-2 py-1 rounded-sm bg-stone-200";
             <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
             <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
         </menu>
+
+        <div>
+        {/* <div className="w-[35rem] mt-16"> */}
+        <Input className={inputStyle} label="Title"/>
+        <Input className={inputStyle} label="Description" textarea/>
+        <Input className={inputStyle} label="Due Date"/>
+        </div>
+
+
+
+
     </div>
 
-    <div className="flex items-center gap-4">
-{/* <p className="text-stone-600 mb-4"> */}
+    {/* <div className="w-[35rem] mt-16">
     <Input className={inputStyle} label="Title"/>
     <Input className={inputStyle} label="Description" textarea/>
     <Input className={inputStyle} label="Due Date"/>
-{/* </p> */}
-    
+    </div> */}
 
-    </div>
+
     {/* <div className="flex items-center gap-4">
         <p className="text-stone-600 mb-4">
             <label>Title</label>
